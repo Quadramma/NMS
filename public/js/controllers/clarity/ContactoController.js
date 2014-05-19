1030c3e63748d6a89bd421f74d349c7079bac660
JLA <arancibiajav@gmail.com>
2014-05-19 20:09:07 -0300

    Contacto filtros actualizados

--- public/js/controllers/clarity/ContactoController.js
+++ public/js/controllers/clarity/ContactoController.js
@@ -1,25 +1,136 @@
-
 angular.module('ContactoNode', [
     "ContactoItemsModule",
     "ContactoEditModule",
     "ContactoCreateModule",
-    "ContactoService"
+    "ContactoService",
+    "ContactoHelperModule",
+    "FrontEndHelperService",
+    "EstadoContactoService",
+    "EmpresaService"
 ]);
 
 angular.module('ContactoItemsModule', []).
-controller('ContactoItemsController', function($scope, $rootScope, Contacto, $timeout) {
+controller('ContactoItemsController', function($scope, $FEHelper
+
+    , EstadoContacto, Empresa, $rootScope, Contacto, $timeout) {
     console.info("[ContactoItemsController]");
-    Contacto.query({
+
+    //
+    $scope.filtro = {
+        EmpresaID: -1,
+        EstadoContactoID: -1,
+        ComienzoDesde: "",
+        ComienzoHasta: ""
+    }
+    //
+    //EmpresaDropdown----------------
+    $FEHelper.bindDropdown({
+        selector: "#empresaDropdown",
+        apiService: Empresa,
+        itemsPerPage: 1000,
         pageNumber: 1,
-        itemsPerPage: 100
-    }, function(res) { //res -> data (page)
-        $scope.items = res.data.Items;
+        waitFor: "ItemsGet",
+        $remoteScope: $scope,
+        fieldID: "EmpresaID",
+        semanticUIInitDelay: 2000,
+        autoBindItem: false, //works for altamodi
+        onQuerySuccess: function(res) {
+            $scope.empresas = res.data.Items;
+        },
+        onQueryFailure: function() {},
+        onInit: function() {
+            return $scope.filtro.EmpresaID;
+        },
+        onChange: function(val) {
+            $scope.filtro.EmpresaID = val;
+            console.info($scope.filtro);
+            $scope.sync();
+        }
     });
+    //
+    //EstadoDropdown----------------
+    $FEHelper.bindDropdown({
+        selector: "#estadoContactoDropdown",
+        apiService: EstadoContacto,
+        itemsPerPage: 1000,
+        pageNumber: 1,
+        waitFor: "ItemsGet",
+        $remoteScope: $scope,
+        fieldID: "EstadoContactoID",
+        semanticUIInitDelay: 1000,
+        autoBindItem: false, //works for altamodi
+        onQuerySuccess: function(res) {
+            $scope.estados = res.data.Items;
+        },
+        onQueryFailure: function() {},
+        onInit: function() {
+            return $scope.filtro.EstadoContactoID;
+        },
+        onChange: function(val) {
+            $scope.filtro.EstadoContactoID = val;
+            console.info($scope.filtro);
+            $scope.sync();
+        }
+    });
+    //
+    //F. Agenda Desde
+    $FEHelper.bindDtp({
+        $item: $scope.item,
+        selector: "#fechaAgendaDesde",
+        autoBindItem: false, //itemField: "Comienzo",
+        defaultValue: null,
+        onChange: function(val) {
+            $scope.filtro.ComienzoDesde = val;
+            $scope.sync();
+        }
+    });
+    //
+    //F. Agenda Hasta
+    $FEHelper.bindDtp({
+        $item: $scope.item,
+        selector: "#fechaAgendaHasta",
+        autoBindItem: false, //itemField: "Comienzo",
+        defaultValue: null,
+        onChange: function(val) {
+            $scope.filtro.ComienzoHasta = val;
+            $scope.sync();
+        }
+    });
+
+    function formatDate(date) {
+        if (date != "") {
+            return moment(date, "DD/MM/YYYY").format("MM/DD/YYYY")
+        }
+        return date;
+    }
+
+    $scope.sync = function() {
+        Contacto.query({
+            pageNumber: 1,
+            itemsPerPage: 100,
+            EstadoContactoID: $scope.filtro.EstadoContactoID,
+            EmpresaID: $scope.filtro.EmpresaID,
+            ComienzoDesde: formatDate($scope.filtro.ComienzoDesde),
+            ComienzoHasta: formatDate($scope.filtro.ComienzoHasta)
+        }, function(res) { //res -> data (page)
+            _.each(res.data.Items, function(item) {
+                var milli = moment(new Date(item.Comienzo).getTime());
+                var formated = milli.format("DD/MM/YYYY");
+                item.Comienzo = formated;
+            });
+            $scope.items = res.data.Items;
+            $scope.$emit("ItemsGet");
+        });
+    }
+    $scope.sync();
 });
 
 
+
 angular.module('ContactoCreateModule', ["ContactoService"]).
-controller('ContactoCreateController', function($scope, $state, $rootScope, Contacto, $timeout) {
+controller('ContactoCreateController', function($scope, $state, ContactoHelper
+
+    , $FEHelper, $rootScope, Contacto, $timeout) {
     console.info("[ContactoCreateController]");
     $scope.trysave = function() {
         console.info("[ContactoCreateController][" + "trysave" + "]");
@@ -41,11 +152,16 @@ controller('ContactoCreateController', function($scope, $state, $rootScope, Cont
 
     }
     //
-    FormValidationDefinition($scope.save, null);
+
+    ContactoHelper.FormValidationDefinition($scope.save, null);
 });
 
+
+
 angular.module('ContactoEditModule', ["ContactoService"]).
-controller('ContactoEditController', function($scope, $state, $rootScope, Contacto, $timeout) {
+controller('ContactoEditController', function($scope, $FEHelper
+
+    , $state, $rootScope, Contacto, EstadoContacto, ContactoHelper, $timeout, Tema) {
     console.info("[ContactoEditController]");
     $scope.trysave = function() {
         console.info("[ContactoEditController][" + "trysave" + "]");
@@ -76,18 +192,110 @@ controller('ContactoEditController', function($scope, $state, $rootScope, Contac
         }
     }
     //
-    FormValidationDefinition($scope.save, null);
+    ContactoHelper.FormValidationDefinition($scope.save, null);
+
+
+    //TemaDropdown----------------
+    $FEHelper.bindDropdown({
+        selector: "#temaDropdown",
+        apiService: Tema,
+        itemsPerPage: 1000,
+        pageNumber: 1,
+        $remoteScope: $scope,
+        waitFor: "ItemGet",
+        fieldID: "TemaID",
+        semanticUIInitDelay: 1000,
+        autoBindItem: true,
+        onQuerySuccess: function(res) {
+            $scope.temas = res.data.Items;
+        },
+        onQueryFailure: function() {}
+    });
+    //EstadoDropdown----------------
+    $FEHelper.bindDropdown({
+        selector: "#estadoContactoDropdown",
+        apiService: EstadoContacto,
+        itemsPerPage: 1000,
+        pageNumber: 1,
+        $remoteScope: $scope,
+        waitFor: "ItemGet",
+        fieldID: "EstadoContactoID",
+        semanticUIInitDelay: 1000,
+        autoBindItem: true,
+        onQuerySuccess: function(res) {
+            $scope.estados = res.data.Items;
+        },
+        onQueryFailure: function() {}
+    });
+
+
     Contacto.get({
         id: $state.params.id
     }, function(res) {
         console.log(res);
-        $scope.item = res.data;
+        $scope.item = res.data[0];
+        $scope.$emit("ItemGet");
+
+        $FEHelper.bindDtp({
+            $item: $scope.item,
+            selector: "#fechaAgenda",
+            itemField: "Comienzo",
+            autoBindItem: true,
+            defaultValue: $scope.item.Comienzo,
+            onChange: function(val) {}
+        });
+
+
     });
 });
 
 
-angular.module('ContactoService', ['ngResource']).
-factory('Contacto', ['$resource', 'AppConfig',
+angular.module("ContactoHelperModule", []).factory("ContactoHelper", [
+
+    function() {
+        return {
+            FormValidationDefinition: function(onSuccess, onFailure) {
+                $('.ui.form')
+                    .form({
+                        asunto: {
+                            identifier: 'asunto',
+                            rules: [{
+                                type: 'empty',
+                                prompt: 'Asunto requerido'
+                            }]
+                        },
+                        proximaccion: {
+                            identifier: 'proximaccion',
+                            rules: [{
+                                type: 'empty',
+                                prompt: 'Proxima Accion requerida'
+                            }]
+                        },
+                        estadoContactoDropdown: {
+                            identifier: 'estadoContactoDropdown',
+                            rules: [{
+                                type: 'empty',
+                                prompt: 'Estado de contacto requerido'
+                            }]
+                        },
+                        fechaAgenda: {
+                            identifier: 'fechaAgenda',
+                            rules: [{
+                                type: 'empty',
+                                prompt: 'Fecha Agenda requerida'
+                            }]
+                        }
+                    }, {
+                        onSuccess: onSuccess,
+                        onFailure: onFailure
+                    });
+            }
+        }
+    }
+]);
+
+
+angular.module('ContactoService', ['ngResource']).factory('Contacto', ['$resource', 'AppConfig',
     function($resource, AppConfig) {
         return $resource(AppConfig.apiClarity + 'contacto/:id', {}, {
             query: {
@@ -120,46 +328,72 @@ factory('Contacto', ['$resource', 'AppConfig',
     }
 ]);
 
+angular.module('EstadoContactoService', ['ngResource']).factory('EstadoContacto', ['$resource', 'AppConfig',
+    function($resource, AppConfig) {
+        return $resource(AppConfig.apiClarity + 'estadocontacto/:id', {}, {
+            query: {
+                method: "GET",
+                isArray: false
+            },
+            get: {
+                method: "GET",
+                isArray: false
+            },
+            update: {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            },
+            delete: {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            },
+            save: {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        });
+    }
+]);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+angular.module('EmpresaService', ['ngResource']).factory('Empresa', ['$resource', 'AppConfig',
+    function($resource, AppConfig) {
+        return $resource(AppConfig.apiClarity + 'empresa/:id', {}, {
+            query: {
+                method: "GET",
+                isArray: false
+            },
+            get: {
+                method: "GET",
+                isArray: false
+            },
+            update: {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            },
+            delete: {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            },
+            save: {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        });
+    }
+]);
 
 
 /*
@@ -337,7 +571,7 @@ function FormValidationDefinition(onSuccess, onFailure) {
 }
 
 */
- /*
+/*
     QUERY RESPONSE FORMAT>
     PETA POCO PAGE FETECHED
     Page = {
@@ -348,4 +582,3 @@ function FormValidationDefinition(onSuccess, onFailure) {
         "Items"
     }
 */
-
